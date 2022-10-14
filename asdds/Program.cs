f972252b6974644cd7f4c2290fd0bfe505b0cf1c
Bladimir Zink <Bladimirzink7@gmail.com>
2022-10-14 11:26:12 -0300

    updated

---
+++ asdds/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITES_PROGRAMACION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
